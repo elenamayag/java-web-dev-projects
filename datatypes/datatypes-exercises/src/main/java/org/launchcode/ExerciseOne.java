b590d4346c8332b0887d59c44f621c3f35c3d0d7
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-12-11 20:16:30 -0600

    studio for classes part1

--- datatypes/datatypes-exercises/src/main/java/org/launchcode/ExerciseOne.java
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/ExerciseOne.java
@@ -1,2 +1,20 @@
-package org.launchcode;public class ExerciseOne {
+package org.launchcode;
+import java.util.Scanner;
+
+// part B Numeric Types
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        Scanner inputLength = new Scanner(System.in);
+        System.out.println("What is the length of your rectangle.");
+
+        String length = inputLength.nextLine();
+        System.out.println("Your rectangles length is: " + length);
+
+        Scanner inputWidth = new Scanner(System.in);
+        System.out.println("What is the width of your rectangle.");
+
+        String width = inputWidth.nextLine();
+        System.out.println("Your rectangles width is: " + width);
+    }
 }
