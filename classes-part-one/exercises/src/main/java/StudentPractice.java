0ac376952a1efcb5cfd09615be6aa8d574990c6f
elenamayag <142683768+elenamayag@users.noreply.github.com>
2024-01-06 12:26:38 -0600

    p1 classes ex

--- classes-part-one/exercises/src/main/java/StudentPractice.java
+++ classes-part-one/exercises/src/main/java/StudentPractice.java
@@ -1,5 +1,30 @@
 public class StudentPractice {
     public static void main(String[] args){
-        //insantiate your Student class below
+        //instantiate your Student class below
+        Student student1 = new Student();
+
+        // Set the properties using setters
+        student1.setName("Alice");
+        student1.setStudentId(12345);
+        student1.setGpa(3.75);
+        student1.setNumberOfCredits(45);
+
+        // Get the properties using getters
+        String name = student1.getName();
+        int studentId = student1.getStudentId();
+        double gpa = student1.getGpa();
+        int numberOfCredits = student1.getNumberOfCredits();
+
+        // Print out the student information
+        System.out.println("Student Name: " + name);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("GPA: " + gpa);
+        System.out.println("Number of Credits: " + numberOfCredits);
     }
 }
+
+//public class Course {
+   // private String topic;
+   // private Teacher instructor;
+    //private ArrayList<Student> enrolledStudents;
+//}
