73b8f02c6b98a387831a20848af2b9dff4cf543e
elenamayag <142683768+elenamayag@users.noreply.github.com>
2024-01-08 19:50:53 -0600

    ex 6

--- unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/exercises/car-exercises/src/test/java/org/launchcode/CarTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CarTest {
-    Car test_car;
+    private Car test_car;
 
     @BeforeEach
-    public void initCar() {
+    public void createCarObject() {
         test_car = new Car("Toyota", "Prius", 10, 50);
     }
 
     //TODO: add emptyTest so we can configure our runtime environment
     @Test
     public void emptyTest() {
-        assertEquals(10, 10, .001);
+        assertEquals( 10, 10, .001);
     }
 
     //TODO: constructor sets gasTankLevel properly
@@ -26,6 +26,8 @@ class CarTest {
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
     //TODO: can't have more gas than tank size, expect an exception
 }
