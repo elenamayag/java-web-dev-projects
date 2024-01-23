07bbb031c1274efbcf00f8a18223311cea7c9eab
elenamayag <142683768+elenamayag@users.noreply.github.com>
2024-01-22 20:09:27 -0600

    ex for exceptions

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -1,27 +1,68 @@
 package org.launchcode;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Test out your Divide() function!
+        int[] integerArray = {1, 1, 2, 3, 5, 8};
 
+        for (int num : integerArray) {
+            System.out.println(num);
+        }
+
+        for (int num : integerArray) {
+            if (num % 2 != 0) {
+                System.out.println(num);
+            }
+        }
+
+         double result = Divide(10, 5);
+        System.out.println("The result is " + result);
+
+        try {
+            result = Divide(10, 0);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        //Testing for case data
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
-        studentFiles.put("Brad", "");
+        studentFiles.put("Brad", "Program.java");
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+
+        for (String student : studentFiles.keySet()) {
+            String fileName = studentFiles.get(student);
+            int points;
+            try {
+                points = CheckFileExtension(fileName);
+                System.out.println(student + " scored " + points + " points.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(student + " error: " + e.getMessage());
+            }
+        }
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static double Divide(int x, int y) throws ArithmeticException {
+        if (y == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return (double) x / y;
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
+    public static int CheckFileExtension(String fileName) {
+        if (fileName == null || fileName.isEmpty()) {
+            throw new IllegalArgumentException("File name is null or empty");
+        }
+
+        if (fileName.endsWith(".java")) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
