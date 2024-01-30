fb6551b23b263dffb7fdfb5fb2c532e40e5e9969
elenamayag <142683768+elenamayag@users.noreply.github.com>
2024-01-29 18:13:44 -0600

    interfaces excerise

---
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/ConeComparator.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+import java.util.Comparator;
+
+public class ConeComparator implements Comparator<Cone> {
+    @Override
+    public int compare(Cone cone1, Cone cone2) {
+        // Return a positive integer if cone1 is more expensive.
+        // Return 0 if both cones have the same cost.
+        return Double.compare(cone1.getCost(), cone2.getCost()); // Return a negative integer if cone1 is cheaper.
+    }
+}
--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Flavor.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Flavor.java
@@ -3,11 +3,7 @@ package org.launchcode;
 import java.util.ArrayList;
 
 public class Flavor extends Ingredient {
-
-
     public Flavor(String aName, double aCost, ArrayList<String> someAllergens){
         super (aName, aCost, someAllergens);
     }
-
-
 }
