da16c099fcb5ffc3507b004b071f0f725a690791
elenamayag <142683768+elenamayag@users.noreply.github.com>
2024-01-31 21:18:55 -0600

    spinning discs studio, done

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -6,5 +6,15 @@ public class Main {
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        // Create a CD object
+        CD myCd = new CD("Music CD", 2000, 225);
+
+        // Create a DVD object
+        DVD myDvd = new DVD("Movie DVD", 11000, 1130);
+
+        myCd.spinDisc();
+        myDvd.spinDisc();
+
     }
 }
